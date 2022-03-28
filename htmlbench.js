815b83f8c85cb4d2a52187456ea51194042909ed
Kirill Myshkin <git@kirill.dvel.ru>
2022-03-29 00:04:35 +0200

    add headings to block elements

--- htmlbench.js
+++ htmlbench.js
@@ -35,13 +35,14 @@ const normal_elements = populate([
     "blockquote", "body", "button", "canvas", "caption", "cite", "code",
     "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog",
     "div", "dl", "dt", "em", "fieldset", "figcaption", "figure", "footer",
-    "form", "head", "header", "hgroup", "html", "i", "iframe", "ins", "kbd",
-    "label", "legend", "li", "main", "map", "mark", "menu", "meter", "nav",
-    "noscript", "object", "ol", "optgroup", "option", "output", "p", "picture",
-    "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script",
-    "section", "select", "slot", "small", "span", "strong", "style", "summary",
-    "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead",
-    "time", "title", "tr", "u", "ul", "var", "video"
+    "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup",
+    "html", "i", "iframe", "ins", "kbd", "label", "legend", "li", "main", "map",
+    "mark", "menu", "meter", "nav", "noscript", "object", "ol", "optgroup",
+    "option", "output", "p", "picture", "pre", "progress", "q", "rp", "rt",
+    "ruby", "s", "samp", "script", "section", "select", "slot", "small", "span",
+    "strong", "style", "summary", "table", "tbody", "td", "template",
+    "textarea", "tfoot", "th", "thead", "time", "title", "tr", "u", "ul", "var",
+    "video"
 ]);
 
 const void_elements = populate([
